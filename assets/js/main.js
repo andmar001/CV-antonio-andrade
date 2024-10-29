13b56426dafe153869f7d2d321bfd8a2818f3ae3
aandrademares <aandrademares@guanajuato.gob.mx>
2024-10-29 14:11:58 -0600

    PDF generate

--- assets/js/main.js
+++ assets/js/main.js
@@ -84,4 +84,43 @@ themeButton.addEventListener('click', () => {
 })
 
 
+// Reduce the size and print on an A4 sheet
+function scaleCv(){
+    document.body.classList.add('scale-cv')
+}
 
+// remove the size when the CV is downloaded
+function removeScale(){
+    document.body.classList.remove('scale-cv')
+}
+
+// Generate PDF
+// PDF generate area 
+let areaCv = document.getElementById('area-cv')
+
+let resumeButton = document.getElementById('resume-button')
+
+// html2pdf options
+let opt = {
+    margin: 0,
+    filename: 'myResume.pdf',
+    image: { type: 'jpeg', quality: 0.98 },
+    html2canvas: { scale: 4 },
+    jsPDF: { format: 'a4', orientation: 'portrait' }
+}
+
+//Function to call areaCV and html2pdf options
+function generateResume(){
+    html2pdf(areaCv, opt)
+}
+// when the button is clicked, it executes the three functions
+resumeButton.addEventListener('click', () => {
+    // 1. The class .scale-cv is added to the body, where it reduces the size of the elements
+    scaleCv()
+
+    // 2. The PDF is generated
+    generateResume()
+
+    // 3. The .scale-cv class is removed from the body after 5 seconds to return to normal size.
+    setTimeout(removeScale, 5000)
+})
